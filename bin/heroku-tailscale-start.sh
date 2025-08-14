9217fe4cc6b13d8d7ea8722d9ab10d2b0e91d2de
CoderMiguel <52347827+CoderMiguel@users.noreply.github.com>
2025-08-13 21:32:24 -0400

    first draft

--- bin/heroku-tailscale-start.sh
+++ bin/heroku-tailscale-start.sh
@@ -1,70 +1,38 @@
 #!/usr/bin/env bash
 
+. ./bin/tailscale_bash_helpers
+
 set -e
 
 if [ -z "$TAILSCALE_AUTH_KEY" ]; then
-  echo "[tailscale]: Will not be available because TAILSCALE_AUTH_KEY is not set"
-  exit 0
-fi
+  log_tailscale_info "skipping tailnet start-up because TAILSCALE_AUTH_KEY is not set"
+#  exit 0
+else
+## TODO: configure allowed dyno types with override ENV variable
+  #tailscaled -cleanup > /dev/null 2>&1
+  #
 
-wait_for_tailscale_running() {
-  timeout=${TAILSCALE_RUNNING_TIMEOUT:-5} # Timeout in seconds
-  interval=0.5  # Interval between checks
+  #set_tailscale_hostname
+  #log "$TAILSCALE_HOSTNAME"
+  #
+  #(tailscaled -verbose ${TAILSCALED_VERBOSE:--1} \
+  #            --tun=userspace-networking \
+  #            --socks5-server=localhost:1055 > /dev/null 2>&1 &)
+  #tailscale up \
+  #  --authkey="${TAILSCALE_AUTH_KEY}" \
+  #  --hostname="$TAILSCALE_HOSTNAME" \
+  #  --advertise-tags=${TAILSCALE_ADVERTISE_TAGS:-} \
+  #  ${TAILSCALE_ADDITIONAL_ARGS:---accept-routes --timeout=15s}
 
-  # convert to milliseconds so we can use integer math
-  timeout_ms=$(awk "BEGIN {print $timeout * 1000}")
-  interval_ms=$(awk "BEGIN {print $interval * 1000}")
+  start_tailscale_daemon #> /dev/null 2>&1 &
+  connect_to_tailnet
 
-  elapsed=0
-
-  while [ "$elapsed" -lt "$timeout_ms" ]; do
-    state=$(tailscale status -json | jq -r .BackendState)
-#    echo "[tailscale]: ($elapsed < $timeout_ms) Current backend state: $state"
-    if [ "$state" = "Running" ]; then
-      return 0
-    fi
-    sleep "$interval"
-    elapsed=$((elapsed + interval_ms))
-  done
-
-  return 1
-}
-
-if [ -z "$TAILSCALE_HOSTNAME" ]; then
-  HEROKU_APP_NAME=${HEROKU_APP_NAME:-$APP_NAME}
-
-  if [ -z "$HEROKU_APP_NAME" ]; then
-    TAILSCALE_HOSTNAME=$(hostname)
+  if tailscale_running; then
+    tailscale_log "Connected to tailnet as hostname=$TAILSCALE_HOSTNAME; SOCKS5 proxy available at localhost:1055"
+    tailscale serve --bg $PORT # expose the Heroku dyno port to the tailnet
+#    exit 0
   else
-    # Only use the first 8 characters of the commit sha.
-    # Swap the . and _ in the dyno with a - since tailscale doesn't
-    # allow for periods.
-    DYNO=${DYNO//./-}
-    DYNO=${DYNO//_/-}
-    TAILSCALE_HOSTNAME="heroku-$HEROKU_APP_NAME-${HEROKU_SLUG_COMMIT:0:8}-$DYNO"
+    tailscale_log "Warning - Backend did not reach 'Running' state within timeout"
+    exit 1
   fi
-else
-  TAILSCALE_HOSTNAME="$TAILSCALE_HOSTNAME"
-fi
-tailscaled -cleanup > /dev/null 2>&1
-
-(tailscaled -verbose ${TAILSCALED_VERBOSE:--1} \
-            --tun=userspace-networking \
-            --socks5-server=localhost:1055 > /dev/null 2>&1 &)
-tailscale up \
-  --authkey="${TAILSCALE_AUTH_KEY}" \
-  --hostname="$TAILSCALE_HOSTNAME" \
-  --advertise-tags=${TAILSCALE_ADVERTISE_TAGS:-} \
-  ${TAILSCALE_ADDITIONAL_ARGS:---accept-routes --timeout=15s}
-
-#export ALL_PROXY=socks5://localhost:1055/
-#export HTTP_PROXY=http://localhost:1055/
-#export http_proxy=http://localhost:1055/ ./my-app
-
-if wait_for_tailscale_running; then
-  echo "[tailscale]: Connected to tailnet as hostname=$TAILSCALE_HOSTNAME; SOCKS5 proxy available at localhost:1055"
-  tailscale serve --bg $PORT
-#  echo "[tailscale]: Status = > $(tailscale status | grep tagged-devices)"
-else
-  echo "[tailscale]: Warning - Backend did not reach 'Running' state within timeout"
 fi
--- bin/heroku-tailscale-test.sh
+++ bin/heroku-tailscale-test.sh
@@ -2,21 +2,44 @@
 
 set -e
 
-function log() {
-  echo "-----> $*"
-}
+. ./logging
+
+#wait_for_tailscale_running() {
+#  timeout=${TAILSCALE_RUNNING_TIMEOUT:-5} # Timeout in seconds
+#  interval=0.5  # Interval between checks
+#
+#  # convert to milliseconds so we can use integer math
+#  timeout_ms=$(awk "BEGIN {print $timeout * 1000}")
+#  interval_ms=$(awk "BEGIN {print $interval * 1000}")
+#
+#  elapsed=0
+#
+#  while [ "$elapsed" -lt "$timeout_ms" ]; do
+#    state=$(tailscale status -json | jq -r .BackendState)
+#
+#    if [ "$state" = "Running" ]; then
+#      return 0
+#    fi
+#
+#    sleep "$interval"
+#
+#    elapsed=$((elapsed + interval_ms))
+#  done
+#
+#  return 1
+#}
 
 if [ -z "$TAILSCALE_AUTH_KEY" ]; then
   log "You need to add TAILSCALE_AUTH_KEY to your environment variables."
 else
   log "Waiting to allow tailscale to finish set up."
-  sleep 10
-  log "Running `tailscale status` You should see your accessible machines on your tailnet."
+  wait_for_tailscale_running
+  log "Running 'tailscale status' You should see your accessible machines on your tailnet."
   tailscale status
 
-  log "Running `proxychains4 -f vendor/proxychains-ng/proxychains.conf curl hello.ts.net` "
-  log 'Things are working if you see <a href="https://hello.ts.net">Found</a>.'
-  proxychains4 -f vendor/proxychains-ng/proxychains.conf curl hello.ts.net
-  log "If you didn't see the Found message, then you may need to add the hello.ts.net machine into your tailnet."
-  log "Test complete. I hope you had your fingers crossed!"
+#  log "Running `proxychains4 -f vendor/proxychains-ng/proxychains.conf curl hello.ts.net` "
+#  log 'Things are working if you see <a href="https://hello.ts.net">Found</a>.'
+#  proxychains4 -f vendor/proxychains-ng/proxychains.conf curl hello.ts.net
+#  log "If you didn't see the Found message, then you may need to add the hello.ts.net machine into your tailnet."
+#  log "Test complete. I hope you had your fingers crossed!"
 fi
