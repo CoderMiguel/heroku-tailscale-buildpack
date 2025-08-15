7109831617997d3a7a34f0ff64ecba1e5c312785
CoderMiguel <52347827+CoderMiguel@users.noreply.github.com>
2025-08-15 13:29:36 -0400

    abstract exposing port to method

--- bin/heroku-tailscale-start.sh
+++ bin/heroku-tailscale-start.sh
@@ -6,30 +6,12 @@ set -e
 
 if [ -z "$TAILSCALE_AUTH_KEY" ]; then
   log_tailscale_info "skipping tailnet start-up because TAILSCALE_AUTH_KEY is not set"
-#  exit 0
 else
-## TODO: configure allowed dyno types with override ENV variable
-  #tailscaled -cleanup > /dev/null 2>&1
-  #
-
-  #set_tailscale_hostname
-  #log "$TAILSCALE_HOSTNAME"
-  #
-  #(tailscaled -verbose ${TAILSCALED_VERBOSE:--1} \
-  #            --tun=userspace-networking \
-  #            --socks5-server=localhost:1055 > /dev/null 2>&1 &)
-  #tailscale up \
-  #  --authkey="${TAILSCALE_AUTH_KEY}" \
-  #  --hostname="$TAILSCALE_HOSTNAME" \
-  #  --advertise-tags=${TAILSCALE_ADVERTISE_TAGS:-} \
-  #  ${TAILSCALE_ADDITIONAL_ARGS:---accept-routes --timeout=15s}
-
-  start_tailscale_daemon #
-
+  # TODO: configure allowed dyno types with override ENV variable
+  start_tailscale_daemon
 
   if connect_to_tailnet; then
-    # expose the Heroku dyno port to the tailnet
-    tailscale serve --bg $PORT
+    expose_heroku_port_to_tailnet
   else
     log_tailscale_error "$(tailscale status)"
     exit 1
